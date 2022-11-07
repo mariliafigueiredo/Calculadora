8f2b51630fb57ca2458ae026b9119c9369fa3414
Marilia Figueiredo <mariliafigueiredo007@hotmail.com>
2022-11-07 18:51:59 -0300

    minha calculadora

---
+++ aula2.py
@@ -0,0 +1,17 @@
+# CALCULADORA
+
+import math
+a = int(input("Digte o primeiro número: "))
+b = int(input("Digte o segundo número: "))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+raiz = math.sqrt(a)
+
+print("Soma: " + str(soma))
+print("Subtracao: " + str(subtracao))
+print("Multiplicacao: " + str(multiplicacao))
+print("Divisao: " + str(divisao))
+print("Raiz: " + str(raiz))
